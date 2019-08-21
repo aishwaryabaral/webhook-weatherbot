492c7dee01b780cfc7c20c26dc762d8ad3758e4c
aishwaryabaral <38627449+aishwaryabaral@users.noreply.github.com>
2019-08-21 19:48:58 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -42,7 +42,7 @@ def makeResponse(req):
     return {
     "fulfillmentText": speech,
     "fulfillmentMessages": speech,
-    "source": "weatheraish"
+    "source": "example.com"
     }
 
 if __name__ == '__main__':
