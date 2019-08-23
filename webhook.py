570557e36cd2b9a6160b302ef61eeb9d4d05158d
aishwaryabaral <38627449+aishwaryabaral@users.noreply.github.com>
2019-08-23 15:15:53 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -29,13 +29,13 @@ def makeResponse(req):
     parameters = result.get("parameters")
     city = parameters.get("geo-city")
     date = parameters.get("date")
-    #condition = ""
+    condition = ""
     if city is None:
         return None
     r=requests.get('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&appid=06f070197b1f60e55231f8c46658d077')
     json_object = r.json()
     weather=json_object['list']
-    condition = ""
+    #condition = ""
     for i in range(0,30):
         if date in weather[i]['dt_txt']:
             condition= weather[i]['weather'][0]['description']
